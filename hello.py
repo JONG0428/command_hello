17760bc25619b1219cf66c2a21cc2aba9607df29
JONG0428 <sju04280@naver.com>
2025-04-11 09:54:11 +0900

    github for git pull

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 // for command line git tutorial
+// local repository modification
 // github modification
 
 print("Tell your world")
 print("Tell his world")
 print("Tell my world")
-
+print("Tell her world")
